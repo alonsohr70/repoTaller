a8d7dc5cce2d5c318e365cdb09a93f54fac67187
Alonso Hernandez R <alonsohernandezr@gmail.com>
2025-08-06 11:52:51 -0600

    trabajo 2 de clase

---
+++ problema02.py
@@ -0,0 +1,19 @@
+#ciclos
+calificacion = 0
+sumacalificacion = 0
+totalcalificaciones = 0
+calificacion = int(input("Introduce una calificación: "))
+while calificacion >= 0 :
+    sumacalificacion = sumacalificacion + calificacion
+    totalcalificaciones = totalcalificaciones + 1
+    calificacion = int(input("Introduce una calificación: "))
+
+if totalcalificaciones == 0 :
+    promedio=0
+    totalcalificaciones = 0
+else:
+    promedio = sumacalificacion / totalcalificaciones
+
+print("Calificaciones leidas:",totalcalificaciones)
+print("Promedio de calificaciones; ",promedio)
+
